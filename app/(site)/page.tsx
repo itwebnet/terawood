89edc4cece9ec3230388da4b5c081d9b0c2732c5
Sergey <dir@itwebnet.ru>
2025-02-13 01:53:03 +0300

    fix title

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -14,10 +14,10 @@ import Blog from "@/components/Blog";
 import Testimonial from "@/components/Testimonial";
 
 export const metadata: Metadata = {
-  title: "Next.js Starter Template for SaaS Startups - Solid SaaS Boilerplate",
+  title: "Terawood - эксклюзивное дерево",
 
   // other metadata
-  description: "This is Home for Solid Pro"
+  description: "Эксклюзивное дерево от поставщика"
 };
 
 export default function Home() {
