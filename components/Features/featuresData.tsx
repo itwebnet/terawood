1fc9d2d51d92c13d5445fcceda2c8b57ce058435
Sergey <dir@itwebnet.ru>
2025-02-13 01:44:57 +0300

    first commit

---
+++ components/Features/featuresData.tsx
@@ -0,0 +1,48 @@
+import { Feature } from "@/types/feature";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    icon: "/images/features/s1.png",
+    title: "Crafted for SaaS",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 2,
+    icon: "/images/features/s2.png",
+    title: "High-quality Design",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 3,
+    icon: "/images/features/s3.png",
+    title: "Next.js 13 + TypeScript",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 4,
+    icon: "/images/features/s4.png",
+    title: "Sanity Blog and Docs",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 5,
+    icon: "/images/features/s5.png",
+    title: "DB, Auth and Stripe",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+  {
+    id: 6,
+    icon: "/images/features/s6.png",
+    title: "Regular Free Updates",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  },
+];
+
+export default featuresData;
